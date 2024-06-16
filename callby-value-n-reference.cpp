f9541d49b17f2cfa8315a3b66796fad2bc2809c2
Aditya Kumar <adi.kr2489@gmail.com>
2024-06-16 10:49:45 +0530

    updated call by value and reference

--- callby-value-n-reference.cpp
+++ callby-value-n-reference.cpp
@@ -18,6 +18,16 @@ void swapPointer (int* a, int* b){   //temp a b
     *a = *b;                         //4   5  5
     *b = temp;                       //4   5  4 
 }
+
+
+// Call by reference using C++ reference Variables
+// int & 
+void swapReferenceVar(int &a, int &b){ //temp a b
+    int temp = a;          //4   4  5   
+    a = b;               //4   5  5
+    b = temp;            //4   5  4 
+    // return a;
+}
 int main(){
     int x=4, y=5;
     cout<<"the sum of 4 and 5 is " <<sum(x,y)<<endl;
@@ -32,5 +42,8 @@ int main(){
     swapPointer(&x, &y);   //This will swap a and b using pointer reference
     cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl;    
     
+    swapReferenceVar(x, y); //This will swap a and b using reference variables
+    // swapReferenceVar(x, y) = 766; //This will swap a and b using reference variables
+    cout<<"The value of x is "<<x<<" and the value of y is "<<y<<endl; 
     return 0;
 }
