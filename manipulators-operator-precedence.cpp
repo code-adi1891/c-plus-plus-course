e96becf1d13525ad55d75ed9b45e7dd5e0d8cc92
Aditya Kumar <adi.kr2489@gmail.com>
2024-06-25 21:39:48 +0530

    updates on manipulators

--- manipulators-operator-precedence.cpp
+++ manipulators-operator-precedence.cpp
@@ -25,14 +25,14 @@ int main(){
    
 
    //**********************MANIPULATORS IN C++********************
-  /* int a=3, b=78, c=1233;
+   int a=3, b=78, c=1233;
+
    cout<<"the value of a without setw is "<<a<<endl;
    cout<<"the value of b without setw is "<<b<<endl;
    cout<<"the value of c without setw is "<<c<<endl;
     cout<<"the value of a with setw is "<<setw(4)<<a<<endl;//is se output ko align kar sakte hai
     cout<<"the value of b with setw is "<<setw(4)<<b<<endl;
     cout<<"the value of c with setw is "<<setw(4)<<c<<endl;
-*/
 
 //********************OPERATOR PRECEDENCE*************************
     int a=3, b=4;
