27d73e59304e33d1f69673e6fe3f3345cc6c1295
Aditya Kumar <adi.kr2489@gmail.com>
2024-07-11 18:25:26 +0530

    sorting array in ascending order

--- array-que-3.cpp
+++ array-que-3.cpp
@@ -1,35 +1,37 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+int main()
+{
     int a;
-    cout<<"enter the total number of elments in the list."<<endl;
-    cin>>a;
+    cout << "enter the total number of elments in the list." << endl;
+    cin >> a;
     int list[a];
-    cout<<"enter all the "<<a<<" elements of the list."<<endl;
+    cout << "enter all the " << a << " elements of the list." << endl;
     for (int i = 0; i < a; i++)
     {
-        cin>>list[i];
+        cin >> list[i];
     }
-    
-    for (int i = 0; i < a - 1; i++) {
-        for (int j = 0; j < a - i - 1; j++) {
-            if (list[j] > list[j + 1]) {
-                // Swap list[j] and list[j + 1]
+    for (int i = 0; i < a - 1; i++)
+    { 
+        for (int j = 0; j < a - i - 1; j++)
+        {
+            if (list[j] > list[j + 1])
+            {
                 int temp = list[j];
                 list[j] = list[j + 1];
                 list[j + 1] = temp;
             }
         }
     }
-
-    cout << "Sorted list in ascending order:" << endl;
-    for (int i = 0; i < a; i++) {
-        cout << list[i] << " ";
+    cout << "sorted list in ascending order: " << endl;
+    for (int i = 0; i < a; i++)
+    {
+        cout << list[i] << endl;
     }
     cout << endl;
     
-    
+
     return 0;
 }
