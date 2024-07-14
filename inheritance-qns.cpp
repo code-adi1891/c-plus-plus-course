cb70449b9839919826cdf77c5055e912f0ea557b
Aditya Kumar <adi.kr2489@gmail.com>
2024-07-14 20:25:39 +0530

    inheritance question

--- inheritance-qns.cpp
+++ inheritance-qns.cpp
@@ -12,10 +12,9 @@ public:
     void get_roll_number(void);
 };
 
-void Student ::set_roll_number(int r)
+void Student ::set_roll_number(int r=0)
 {
     roll_number = r;
-    float roll_number = 0;
 }
 
 void Student ::get_roll_number()
@@ -35,7 +34,7 @@ public:
     void get_marks(void);
 };
 
-void Exam ::set_marks(float m1, float m2, float m3)
+void Exam ::set_marks(float m1=0, float m2=0, float m3=0) //default constructor, default marks of all the subjects is 0
 {
     maths = m1;
     physics = m2;
@@ -66,8 +65,8 @@ public:
 int main()
 {
     Result aditya;
-    aditya.set_roll_number(002);
-    aditya.set_marks(94.2, 90.4, 10);
+    aditya.set_roll_number();
+    aditya.set_marks();
     aditya.display_results();
     return 0;
 }
